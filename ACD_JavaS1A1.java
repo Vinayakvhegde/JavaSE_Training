10aa426eb92ad9c78c5b9c7699609c21f655383a
Vinayakvhegde <vinayakvhegde@gmail.com>
2016-05-26 23:35:03 +0200

    Revert "Revert "S1Assignments""
    This reverts commit f30641dfc2100c33849fb43627aca7401fabb9a1.

---
+++ ACD_JavaS1A1.java
@@ -0,0 +1,12 @@
+
+public class ACD_JavaS1A1 {
+	public static void main (String args[]) {
+		// This is a program to display the sum of 2 numbers
+		int num1 = 45 ;
+		float num2 = 3.45f ;
+		int sum ;
+		sum = (int) (num1 + num2) ;
+		System.out.println("The sum of the numbers is: " + sum);
+	}
+
+}
---
+++ ACD_JavaS1A3.java
@@ -0,0 +1,10 @@
+
+public class ACD_JavaS1A3 {
+	public static void main (String args[]) {
+		// This is a program to show the variables that can't be int type
+		float simpleIntRate = 4.5f ;
+		float weightOfABox  = 24.54f ;
+		System.out.println("The fractional value can't be assigned to int type, such as - simple Interest rate and weight of a box !" );
+	}
+
+}
