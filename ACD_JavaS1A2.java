f30641dfc2100c33849fb43627aca7401fabb9a1
Vinayakvhegde <vinayakvhegde@gmail.com>
2016-05-26 23:34:50 +0200

    Revert "S1Assignments"
    This reverts commit cdde8a4401aae54681dd573d04834f4a80667a44.

--- ACD_JavaS1A2.java
+++
@@ -1,9 +0,0 @@
-
-public class ACD_JavaS1A2 {
-	public static void main (String args[]) {
-		// This is a program to display variable declaration and assignment of values
-		int varA, varB = 4, varC ;
-		System.out.println("The value of varB: " + varB);
-	}
-
-}
